09ac6412737c1c2d44e031154bb8647a5528d8e5
Noah Clark <noahammon00@gmail.com>
2023-03-01 16:17:57 -0700

    added movie titles to our popup message

--- movies.js
+++ movies.js
@@ -2,16 +2,18 @@ let message = document.querySelector('#message');
 
 const deleteMovie = event => {
     event.target.parentNode.remove();
-    message.textContent = 'Movie deleted';
+    message.textContent = `${event.target.previousSibling.textContent} deleted`;
+    revealMessage(event);
 }
 
 const crossOffMovie = event => {
     event.target.classList.toggle('checked');
     if (event.target.classList.contains('checked')) {
-        message.textContent = 'Movie watched';
+        message.textContent = `${event.target.textContent} watched`;
     } else {
-        message.textContent = 'Movie added back';
+        message.textContent = `${event.target.textContent} added back`;
     }
+    revealMessage(event);
 }
 
 const addMovie = event => {
@@ -35,3 +37,9 @@ const addMovie = event => {
 
 document.querySelector('form').addEventListener('submit', addMovie);
 
+function revealMessage() {
+    message.classList.remove('hide');
+    setTimeout(() => {
+        message.classList.add('hide');
+    }, 1000);
+}
